1b92f132d930ef1a44e4ec9d349faab275df969e
ibikadaeze <ibik@astro.utoronto.ca>
2024-08-31 22:49:33 -0400

    Initial commit

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  // Validate and store the data in a database or send via email
+  // For example, send an email using PHP's mail() function:
+  $to = 'your_email@example.com';
+  $subject = 'Contact Form Submission';
+  $body = "Name: $name\nEmail: $email\nMessage: $message";
+  mail($to, $subject, $body);
+?>
